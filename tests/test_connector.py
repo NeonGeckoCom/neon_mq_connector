805622952ea5630aea7fcc98cca3569de131885f
Daniel McKnight <daniel@neongecko.com>
2021-08-17 11:59:32 -0700

    Move consumer definition inside ConsumerThread init to resolve errors on channel close (https://github.com/pika/pika/issues/1144#issuecomment-521026912) Add error_func to handle any exceptions raised in consumers Add unit testing of error callback TODO: Drop compatibility checks before push to master

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -40,18 +40,29 @@ class MQConnectorChild(MQConnector):
         self.func_2_ok = True
         channel.basic_ack(delivery_tag=method.delivery_tag)
 
+    def callback_func_error(self, channel, method, properties, body):
+        raise Exception("Exception to Handle")
+
+    def handle_error(self, thread: ConsumerThread, exception: Exception):
+        self.exception = exception
+
     def __init__(self, config: dict, service_name: str):
         super().__init__(config=config, service_name=service_name)
         self.vhost = '/test'
         self.func_1_ok = False
         self.func_2_ok = False
+        self.exception = None
         self.connection = self.create_mq_connection(vhost=self.vhost)
-        self.consumers = dict(test1=ConsumerThread(connection=self.create_mq_connection(vhost=self.vhost),
+        self.consumers = dict(test1=ConsumerThread(connection_params=self.get_connection_params(vhost=self.vhost),
                                                    queue='test',
                                                    callback_func=self.callback_func_1),
-                              test2=ConsumerThread(connection=self.create_mq_connection(vhost=self.vhost),
+                              test2=ConsumerThread(connection_params=self.get_connection_params(vhost=self.vhost),
                                                    queue='test1',
-                                                   callback_func=self.callback_func_2)
+                                                   callback_func=self.callback_func_2),
+                              error=ConsumerThread(connection_params=self.get_connection_params(vhost=self.vhost),
+                                                   queue='error',
+                                                   callback_func=self.callback_func_error,
+                                                   error_func=self.handle_error)
                               )
 
 
@@ -61,12 +72,12 @@ class MQConnectorChildTest(unittest.TestCase):
         file_path = 'config.json' if os.path.isfile("config.json") else "~/.local/share/neon/credentials.json"
         cls.connector_instance = MQConnectorChild(config=Configuration(file_path=file_path).config_data,
                                                   service_name='test')
-        cls.connector_instance.run_consumers(names=('test1', 'test2'))
+        cls.connector_instance.run_consumers(names=('test1', 'test2', 'error'))
 
     @classmethod
     def tearDownClass(cls) -> None:
         try:
-            cls.connector_instance.stop_consumers(names=('test1', 'test2'))
+            cls.connector_instance.stop_consumers(names=('test1', 'test2', 'error'))
         except ChildProcessError as e:
             LOG.error(e)
         try:
@@ -74,15 +85,15 @@ class MQConnectorChildTest(unittest.TestCase):
         except pika.exceptions.StreamLostError as e:
             LOG.error(f'Consuming error: {e}')
 
-    def test_01_not_null_service_id(self):
+    def test_not_null_service_id(self):
         self.assertIsNotNone(self.connector_instance.service_id)
 
     @pytest.mark.timeout(30)
-    def test_02_connection_alive(self):
+    def test_connection_alive(self):
         self.assertIsInstance(self.connector_instance.consumers['test1'], ConsumerThread)
 
     @pytest.mark.timeout(30)
-    def test_03_produce(self):
+    def test_produce(self):
         self.channel = self.connector_instance.connection.channel()
         self.channel.basic_publish(exchange='',
                                    routing_key='test',
@@ -102,3 +113,18 @@ class MQConnectorChildTest(unittest.TestCase):
         time.sleep(3)
         self.assertTrue(self.connector_instance.func_1_ok)
         self.assertTrue(self.connector_instance.func_2_ok)
+
+    @pytest.mark.timeout(30)
+    def test_error(self):
+        self.channel = self.connector_instance.connection.channel()
+        self.channel.basic_publish(exchange='',
+                                   routing_key='error',
+                                   body='test',
+                                   properties=pika.BasicProperties(
+                                       expiration='3000'
+                                   ))
+        self.channel.close()
+
+        time.sleep(3)
+        self.assertIsInstance(self.connector_instance.exception, Exception)
+        self.assertEqual(str(self.connector_instance.exception), "Exception to Handle")
